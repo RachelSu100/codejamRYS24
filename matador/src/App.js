c7002eca738915661b4b4db97be844f67071cf9a
fathisahar <sahar.fathi@mail.mcgill.ca>
2024-11-23 11:58:48 -0500

    video + routing to new page

--- matador/src/App.js
+++ matador/src/App.js
@@ -1,15 +1,17 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Homepage from './Homepage';
+import Chatpage from './Chatpage';  
 
-import Homepage from "./Homepage";
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Homepage />
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/chat-page" element={<Chatpage />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
--- matador/src/Homepage.js
+++ matador/src/Homepage.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import './Homepage.css';
+import cars from './resources/cars.mp4';
+import { useNavigate } from 'react-router-dom';
 
+const Homepage = () => {
+    const navigate = useNavigate();
 
-const Homepage = () => (
-    <main>
-      <p className="tagline-text">Find Your Dream Car.</p>
-      <button className="get-started-button">Get Started Now</button>
-    </main>
-  );
-  
-  export default Homepage;
-  
+    const handleClick = () => {
+        navigate('/chat-page');
+    };
+
+    return (
+        <main className="main-container">
+            <div className='video-container'>
+                <video className='background-video' autoPlay loop muted>
+                    <source src={cars} type='video/mp4' />
+                </video>
+            </div>
+
+            <div className='text-container'>
+                <p className="tagline-text">Find your dream car.</p>
+                <button 
+                    className="get-started-button" 
+                    onClick={handleClick}
+                >
+                    Get started now
+                </button>
+            </div>
+        </main>
+    );
+};
+
+export default Homepage;
