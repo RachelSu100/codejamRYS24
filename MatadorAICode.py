1d205e46acb93083d986d9a350cefe241efa6997
RachelSu100 <rachelsu100@gmail.com>
2024-11-23 13:06:00 -0500

    streamlit version

---
+++ MatadorAICode.py
@@ -0,0 +1,13 @@
+# requirements 
+%pip install pandas --quiet 
+%pip install flask requests --quiet
+%pip install ollama --quiet
+%pip install streamlit streamlit-chat --quiet
+%pip install langchain --quiet
+%pip install faiss-cpu --quiet
+%pip install transformers --quiet
+%pip install ctransformers --quiet
+%pip install requests --quiet
+%pip install -U langchain-community --quiet
+
+print("Requirements installed successfully!")
